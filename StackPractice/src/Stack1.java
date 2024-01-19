7b05fb9942033d60f1f91fc12fb239a53d580d24
rohit kumar <rraj70775@gmail.com>
2024-01-19 16:01:23 +0530

    Stack Practice

---
+++ StackPractice/src/Stack1.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class Stack1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<Integer> st = new Stack<>();
+		st.push(21);
+		st.push(23);
+		st.push(11);
+		st.push(15);
+		
+		
+//		int 
+		while(!st.isEmpty()) {
+			System.out.println(st.pop());
+		}
+		
+		LinkedList<Integer> ll = new LinkedList<>();
+//		ll.offer( )
+		
+		
+		
+
+	}
+
+}
